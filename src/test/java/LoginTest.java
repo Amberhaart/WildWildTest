0878e796014e9e728778bb2ac410272491d136a0
Roland Bak <bakroland0@gmail.com>
2019-03-06 15:20:25 +0100

    feat(): utilized login method from Login class

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,14 +1,32 @@
+import configuration.Config;
+import keywords.Login;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class LoginTest {
 
+  protected static WebDriver driver;
+
+  @BeforeClass
+  public static void setUp() {
+    System.setProperty("webdriver.gecko.driver", "/Users/Roland/Gecko-drivers/Mozilla/geckodriver");
+    driver = new FirefoxDriver();
+    driver.manage().window().maximize();
+    driver.get(Config.url + "login/");
+  }
+
+  @AfterClass
+  public static void tearDown() {
+    driver.quit();
+  }
+
   @Test
-  public void openLogin() {
-  System.setProperty("webdriver.gecko.driver", "/Users/Roland/Gecko-drivers/Mozilla/geckodriver");
-  WebDriver driver = new FirefoxDriver();
-  driver.manage().window().maximize();
-  driver.get("http://localhost:3001/");
+  public void LoginTest() {
+    Login.login(driver);
+    Assert.assertTrue(Login.validateLoggedInState(driver));
   }
 }
