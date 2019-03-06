042c8e2c17aed38753201d234b60d81f9e26f5d4
Andras Gerencser <andras.gerencser@gmail.com>
2019-03-06 18:58:51 +0100

    feat(): add getReadyByTime() method

--- src/test/java/keywords/BuildTime.java
+++ src/test/java/keywords/BuildTime.java
@@ -1,4 +1,20 @@
 package keywords;
 
+import configuration.Config;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class BuildTime {
+
+  public static String getReadyByTime(WebDriver driver) {
+    String readyByTimeString;
+    WebDriverWait wait = new WebDriverWait(driver, Config.timeOutInSeconds);
+    WebElement readyByTime = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div/main/div/div[2]/div/div[2]/div/div[1]/p")));
+    readyByTimeString = readyByTime.getText();
+    System.out.println(readyByTimeString);
+    return readyByTimeString;
+  }
 }
