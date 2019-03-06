72a45c8c1e9aa0fcb8070ad9851fb498db68ab93
Andras Gerencser <andras.gerencser@gmail.com>
2019-03-06 13:56:42 +0100

    init()

---
+++ src/test/java/LogoutTest.java
@@ -0,0 +1,36 @@
+import configuration.Config;
+import keywords.Logout;
+import org.junit.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class LogoutTest {
+
+  private static WebDriver driver;
+
+  @BeforeClass
+  public static void initialize() {
+    System.setProperty(Config.webDriverName, Config.webDriverPath);
+    driver = new FirefoxDriver();
+    driver.manage().deleteAllCookies();
+    driver.manage().window().maximize();
+  }
+
+  @Before
+  public void setUp() {
+    // here comes the login method
+  }
+
+  @AfterClass
+  public static void tearDown(){
+    driver.quit();
+  }
+
+  @Test
+  public void logout(){
+    Logout.logout(driver);
+    Assert.assertTrue(Logout.isLoggedOut(driver));
+  }
+
+}
