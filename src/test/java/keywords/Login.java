3339f0e44325067569b0d83a8ff3dc0de7d1b30b
Roland Bak <bakroland0@gmail.com>
2019-03-06 15:20:07 +0100

    feat(): created method to log in and search for logout button

--- src/test/java/keywords/Login.java
+++ src/test/java/keywords/Login.java
@@ -1,4 +1,33 @@
 package keywords;
 
+import configuration.Config;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class Login {
+
+  public static void login(WebDriver driver) {
+    WebDriverWait wait = new WebDriverWait(driver, Config.timeOutInSeconds);
+
+    WebElement usernameInput = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"app\"]/main/div/form/input[1]")));
+    usernameInput.sendKeys(Config.userName);
+
+    WebElement passwordInput = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"app\"]/main/div/form/input[2]")));
+    passwordInput.sendKeys(Config.password);
+
+    WebElement loginBtn = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"app\"]/main/div/form/button")));
+    loginBtn.click();
+  }
+
+  public static boolean validateLoggedInState(WebDriver driver) {
+    WebDriverWait wait = new WebDriverWait(driver, Config.timeOutInSeconds);
+
+    WebElement logoutBtn = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"app\"]/header/nav/button[2]")));
+
+    return logoutBtn.isDisplayed();
+  }
+
 }
