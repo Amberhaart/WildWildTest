3ef0641d082b8c28628e2d7063cee2ff1c9a8de5
Andras Gerencser <andras.gerencser@gmail.com>
2019-03-06 13:53:58 +0100

    init()

---
+++ src/test/java/keywords/Logout.java
@@ -0,0 +1,17 @@
+package keywords;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Logout {
+
+  public static void logout(WebDriver driver) {
+    WebElement logoutBtn = driver.findElement(By.xpath("/html/body/div/header/nav/button[2]"));
+    logoutBtn.click();
+  }
+
+  public static Boolean isLoggedOut(WebDriver driver) {
+    return driver.findElements(By.xpath("/html/body/div/main/div/form/button")).size() != 0;
+  }
+}
