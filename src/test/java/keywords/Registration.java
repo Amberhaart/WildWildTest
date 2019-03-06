5eee75fe68ab14de44f3629f4d633d93460a3126
Noemi Csaszari <csaszari.noemi@gmail.com>
2019-03-06 13:44:33 +0100

    feat(Registration): add clickOnRegisterButton)

--- src/test/java/keywords/Registration.java
+++ src/test/java/keywords/Registration.java
@@ -1,4 +1,19 @@
 package keywords;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import configuration.Config;
+
+
 public class Registration {
+  public static void clickOnRegisterButton(WebDriver driver){
+    WebDriverWait wait = new WebDriverWait(driver,Config.timeOutInSeconds);
+    wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"app\"]/header/nav/button[@name=\"/register\"]"))).click();
+  }
+
+  public static void getUserNameField(WebDriver driver){
+
+  }
 }
