3cfd36edcdc75d86f8aa2925ef99b5352a747fb4
Andras Gerencser <andras.gerencser@gmail.com>
2019-03-06 17:38:38 +0100

    fix(): getResourceCount() method

--- src/test/java/keywords/ResourceGeneration.java
+++ src/test/java/keywords/ResourceGeneration.java
@@ -1,15 +1,29 @@
 package keywords;
 
+import configuration.Config;
 import enums.ResourceNameEnum;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 public class ResourceGeneration {
 
-  public static int getResourceCount (WebDriver driver, ResourceNameEnum resourceNameEnum) {
+  public static int getResourceCount (WebDriver driver, ResourceNameEnum resourceNameEnum) throws InterruptedException {
+    WebElement resourceCount;
+    int resourceCountInt;
+    driver.manage().timeouts().implicitlyWait(Config.timeOutInSeconds, TimeUnit.SECONDS);
+    WebDriverWait wait = new WebDriverWait(driver, Config.timeOutInSeconds);
+    Thread.sleep(1000);
     if (resourceNameEnum.getResourceName() == "pineapple") {
-      return Integer.parseInt(driver.findElement(By.xpath("/html/body/div/main/div/div[1]/div[1]/div[2]/div[1]/ul/li[1]/span")).getText());
+      resourceCount = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div/main/div/div[1]/div[1]/div[2]/div[1]/ul/li[1]/span")));
+    } else {
+      resourceCount = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div/main/div/div[1]/div[1]/div[2]/div[2]/ul/li[1]/span")));
     }
-    return Integer.parseInt(driver.findElement(By.xpath("/html/body/div/main/div/div[1]/div[1]/div[2]/div[2]/ul/li[1]/span")).getText());
+    resourceCountInt = Integer.parseInt(resourceCount.getText());
+    return resourceCountInt;
   }
 }
