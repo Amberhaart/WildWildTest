b7a821cc2d7e387129dad4b784f1d084ea2943b1
Andras Gerencser <andras.gerencser@gmail.com>
2019-03-06 15:36:32 +0100

    feat(): add getResourceCount method

--- src/test/java/keywords/ResourceGeneration.java
+++ src/test/java/keywords/ResourceGeneration.java
@@ -1,6 +1,15 @@
 package keywords;
 
+import enums.ResourceNameEnum;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class ResourceGeneration {
 
-  public static int getResourceCount()
+  public static int getResourceCount (WebDriver driver, ResourceNameEnum resourceNameEnum) {
+    if (resourceNameEnum.getResourceName() == "pineapple") {
+      return Integer.parseInt(driver.findElement(By.xpath("/html/body/div/main/div/div[1]/div[1]/div[2]/div[1]/ul/li[1]/span")).getText());
+    }
+    return Integer.parseInt(driver.findElement(By.xpath("/html/body/div/main/div/div[1]/div[1]/div[2]/div[2]/ul/li[1]/span")).getText());
+  }
 }
