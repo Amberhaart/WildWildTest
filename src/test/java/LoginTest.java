0da12b113dc3d68ee67fd2bbed9095ec3e2f8e15
Bak Roland <42812441+Amberhaart@users.noreply.github.com>
2019-03-07 10:41:03 +0100

    Update LoginTest.java

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,15 +1,15 @@
 import configuration.Config;
 import keywords.Login;
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import keywords.Logout;
+import org.junit.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class LoginTest {
 
   protected static WebDriver driver;
+  protected static String falseUsername = "Trükkös hekker";
+  protected static String falsePassword = "kamujelszó";
 
   @BeforeClass
   public static void setUp() {
@@ -24,9 +24,39 @@ public class LoginTest {
     driver.quit();
   }
 
+  @After
+  public void reset() {
+    Login.clearInputFields(driver);
+  }
+
   @Test
-  public void LoginTest() {
+  public void LoginWithValidCredentials() {
     Login.login(driver, Config.userName, Config.password);
     Assert.assertTrue(Login.validateLoggedInState(driver));
+    Logout.logout(driver);
+  }
+
+  @Test
+  public void LoginWithInvalidUsername() {
+    Login.login(driver, falseUsername, falsePassword);
+    Assert.assertEquals("No such user: " + falseUsername + "!", Login.validateErrorMessage(driver));
+  }
+
+  @Test
+  public void LoginWithInvalidPassword() {
+    Login.login(driver, Config.userName, falsePassword);
+    Assert.assertEquals("Wrong password!", Login.validateErrorMessage(driver));
+  }
+
+  @Test
+  public void LoginWithNoPassword() {
+    Login.login(driver, Config.userName, "");
+    Assert.assertEquals("Please fill out password!", Login.validateErrorMessage(driver));
+  }
+
+  @Test
+  public void LoginWithNoUsername() {
+    Login.login(driver, "", Config.password);
+    Assert.assertEquals("Please fill out username!", Login.validateErrorMessage(driver));
   }
 }
